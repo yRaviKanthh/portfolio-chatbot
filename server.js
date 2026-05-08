bd4f087edadbdab916f0c2145aadaa331d63a394
RaviKanth <yravikanththarun@gmail.com>
2026-05-08 18:17:59 +0530

    temp rename

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("Public"));
+app.use(express.static("temp"));
 
 const client = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
