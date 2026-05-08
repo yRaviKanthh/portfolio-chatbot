43298a1b628be78270ba01b93f80fb6f611a0b9f
RaviKanth <yravikanththarun@gmail.com>
2026-05-08 17:30:53 +0530

    fixed render port

--- server.js
+++ server.js
@@ -67,6 +67,8 @@ app.post("/chat", async (req, res) => {
 
 });
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
   console.log("Server running");
 });
